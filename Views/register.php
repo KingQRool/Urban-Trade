7cb85da9a9dc1d4429102f04a2853eb4966d6650
EYIYER <eyiyer2004@gmail.com>
2022-10-28 13:33:48 -0500

    a

--- Views/register.php
+++ Views/register.php
@@ -17,16 +17,24 @@
     <div class="form__field">
       <label for="login__username"><svg class="icon">
           <use xlink:href="#icon-user"></use>
-        </svg><span class="hidden">Username</span></label>
-      <input autocomplete="username" id="login__username" type="text" name="username" class="form__input" placeholder="Username" required>
+        </svg><span class="hidden">Nombre</span></label>
+      <input id="login__username" type="text" name="nombreu" class="form__input" placeholder="Username" required>
+    </div>
+    
+    <div class="form__field">
+      <label for="login__password"><svg class="icon">
+          <use xlink:href="#icon-email"></use>
+        </svg><span class="hidden">Correo</span></label>
+      <input id="login__email" type="password" name="emailu" class="form__input" placeholder="Correo" required>
     </div>
 
     <div class="form__field">
       <label for="login__password"><svg class="icon">
           <use xlink:href="#icon-lock"></use>
         </svg><span class="hidden">Password</span></label>
-      <input id="login__password" type="password" name="password" class="form__input" placeholder="Password" required>
+      <input id="login__password" type="password" name="contrasenau" class="form__input" placeholder="Contraseña" required>
     </div>
+
 
     <div class="form__field">
       <input type="submit" value="Sign In">
